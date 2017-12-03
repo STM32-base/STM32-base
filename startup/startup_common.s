07bed281c5c13959be6856276439c04828f5e67c
Thomas Gravekamp <me@thomas-gravekamp.nl>
2017-12-03 17:16:12 +0100

    Complete documentation for startup files.

--- startup/startup_common.s
+++ startup/startup_common.s
@@ -23,37 +23,25 @@
 .weak Reset_Handler
 .type Reset_Handler, %function
 Reset_Handler:
+// Copy the initialized global variables to RAM
     movs r0, #0
     ldr  r1, = __data_start
     ldr  r2, = __data_end
     ldr  r3, = __data_flash_start
-    b    LoopCopyDataInit
+    b    LoopCopyData
 
-// Copy over the initialized global variables
-CopyDataInit:
+CopyData:
     ldr  r4, [r3, r0]
     str  r4, [r1, r0]
     adds r0, r0, #4
 
-LoopCopyDataInit:
+LoopCopyData:
     adds r4, r1, r0
     cmp  r4, r2
-    bcc  CopyDataInit
-
-    movs r0, #0
-    ldr  r1, = __bss_start
-    ldr  r2, = __bss_end
-    b    LoopFillZerobss
-
-FillZerobss:
-    str  r0, [r1]
-    adds r1, r1, #4
-
-LoopFillZerobss:
-    cmp  r1, r2
-    bcc  FillZerobss
+    bcc  CopyData
 
 .ifdef __ccmdata_start
+// Copy the initialized global variables to CCM
     movs r0, #0
     ldr  r1, = __ccmdata_start
     ldr  r2, = __ccmdata_end
@@ -70,6 +58,20 @@ LoopCopyCCMData:
     cmp  r4, r2
     bcc  CopyCCMData
 .endif
+
+// Fill uninitialized variables with zeros
+    movs r0, #0
+    ldr  r1, = __bss_start
+    ldr  r2, = __bss_end
+    b    LoopFillZerobss
+
+FillZerobss:
+    str  r0, [r1]
+    adds r1, r1, #4
+
+LoopFillZerobss:
+    cmp  r1, r2
+    bcc  FillZerobss
 
     bl SystemInit
     bl __libc_init_array
